14cb3ebab9d3a096d698aaa7cd683da40643878f
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-03-22 13:03:50 -0400

    3. Controllers in ASP.NET MVC 4

--- OdeToFood/OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/OdeToFood/Controllers/CuisineController.cs
@@ -1,18 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using OdeToFood.Filters;
+using System;
 using System.Web.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    //    [Authorize]
     public class CuisineController : Controller
     {
         // GET: Cuisine
-        public ActionResult Search(string name)
+        //        [HttpPost]
+        //        [Authorize(Roles="Admin")]
+//        [Authorize]
+[Log]
+        public ActionResult Search(string name = "french")
         {
+            throw new Exception("Something terrible has happened");
             var message = Server.HtmlEncode(name);
             return Content(message);
+//            return RedirectToAction("Index", "Home", new {name = message});
+//            return RedirectToRoute("Default", new {controller="Home", action="About"});
+//            return File(Server.MapPath("~/Content/site.css"), "text/css");
+//            return Json(new {Message = message, Name = "Marcelo"}, JsonRequestBehavior.AllowGet);
         }
+
+//        [HttpGet]
+//        public ActionResult Search()
+//        {
+//            return Content("Search");
+//        }
     }
 }
